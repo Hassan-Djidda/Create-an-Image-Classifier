bc8ea564c4fa3febb2dc6710f8c5c25cbc086683
HassanSoftDev <hassanmahamatdjidda47@gmail.com>
2023-03-25 12:36:58 +0100

    Add file in the reposotory

---
+++ predict.py
@@ -0,0 +1,137 @@
+import torch 
+from torch import nn, optim
+from torchvision import datasets, transforms, models
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline 
+import json
+import argparse
+
+#To get argument fromm the CMD
+
+def get_input_arg():
+    parser = argparse.ArgumentParser(description='Image Classifier')
+
+    parser.add_argument("image_dir", required=True, type=str, help="path to folder of images")
+    parser.add_argument('--checkpoint',required=True, type = str, default='densenet121', help=" CNN model architecture")
+    parser.add_argument('--top_k', action='store_true', type=int, default=3, help="Learning rate of the model")
+    parser.add_argument('--category_name', action='store_true',type=str, default='cat_to_name.json', help="To save the directory")
+    parser.add_argument('--gpu', action='store_true',type=str, default='cuda', help="Set to gpu mode")
+    
+    return parser.parse_args()
+
+#To process the image for feeding the network
+
+def process_image(path_image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    image = Image.open(path_image)
+    new_size = (256, 256)
+    resized_image = image.resize(new_size)
+    np_image = np.array(resized_image)
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = (np_image - mean) / std
+    transposed_image = image.transpose()
+    
+    return transposed_image
+
+#To print an image tensor
+def imshow(image, ax=None, title=None):
+    """Imshow for Tensor."""
+    if ax is None:
+        fig, ax = plt.subplots()
+    
+    # PyTorch tensors assume the color channel is the first dimension
+    # but matplotlib assumes is the third dimension
+    image = image.transpose((1, 2, 0))
+    
+    # Undo preprocessing
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+    
+    # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
+    image = np.clip(image, 0, 1)
+    
+    ax.imshow(image)
+    
+    return ax
+
+
+#To make prediction
+
+def predict(image_path, model, topk=5):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+    
+    images = process_image(image_path)
+    image_tensor = torch.from_numpy(images).type(torch.FloatTensor)
+    image_tensor = image_tensor.unsqueeze(0)
+    image_tensor = image_tensor.to(device)
+    predicted = model.forward(image_tensor)
+    ps = torch.exp(predicted)
+    top_ps, top_idices = predicted.topk(topk)
+    
+    top_ps = top_ps.numpy().squeeze()
+    top_indices = top_indices.numpy().squeeze()
+    
+    idx_to_class = {i: j for i, j in model.class_to_idx.item()}
+    top_classes = [idx_to_class[idx] for idx in top_indices]
+    
+    return top_ps, top_classes
+
+# Display category names
+
+def display_cat(img_path, top_ps, top_class ,json_file):
+    image_name = []
+
+    for key, value in json.items():
+        for name in top_class:
+            if name == value:
+                flower_name.append(value)
+      
+    print(image_name) 
+
+#To print an image with its probalilities
+
+def display_image(img_path, top_ps, top_class ,json_file):
+    img_name = json[img_path.split('/')[1]].title()
+    
+    fig, axes = plt.subplot(1,2, figsize=(14, 8))
+    axes[0].imshow(top_class)
+    axes[0].set(title=img_name)
+    axes[0].axes('off')
+    
+    axes[1].barh(top_class, ps, aligned='Center')
+    axes[1].set(title='Prediction')
+    axes[1].set_xticks('Probabilities')
+    
+    plt.tight_layout()
+    plt.show()
+    
+    
+#Make prediction
+
+    
+def main():
+    in_args = get_input_arg()
+    image_path = in_args.img_dir
+    checkpoint = in_args.checkpoint
+    top_k = in_args.top_k
+    cat_name = in_args.category_name
+    gpu_mode = in_args.gpu
+    
+    if top_k:
+        top_ps, top_class = predict(image_dir, checkpoint, top_k)
+        print(top_class)
+    if cat_name:
+        top_ps, top_class = predict(image_dir, checkpoint)
+        display_cat(top_class,cat_name)
+    top_ps, top_class =predict(image_dir, chekpoint)
+    display_image(image_dir, top_ps, top_class, json_file)
+
+if __name__ == "__main__":
+    main()
---
+++ train.py
@@ -0,0 +1,143 @@
+
+#My functions
+
+#To get arguments from the CMD
+def get_input_arg():
+    
+    parser = argparse.ArgumentParser(description='Image Classifier')
+
+    parser.add_argument("image_dir", required=True, type=str, help="path to folder of images")
+    parser.add_argument('--arch',action='store_true', type = str, default='densenet121', help=" CNN model architecture")
+    parser.add_argument('--learning_rate', action='store_true', type=int, default=0.001, help="Learning rate of the model")
+    parser.add_argument('--save_dir', action='store_true',type=str, default='chekpoint.pth', help="To save the directory")
+    parser.add_argument('--epochs', action='store_true',type=str, default=20, help="Number of epochs")
+    parser.add_argument('--hidden_units', action='store_true',type=int, default=512, help="Hidden layer features")
+    parser.add_argument('--gpu', action='store_true',type=str, default='cuda', help="Set to gpu mode")
+    
+    return parser.parse_args()
+
+#To load the and the datalaoder
+def data_loader(image_path):
+    
+    train_dir = image_path
+    test_dir = image_path
+        
+    train_transform = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize([0.485, 0.456, 0.406],
+                                                            [0.229, 0.224, 0.225])])
+    test_transform = transforms.Compose([transforms.Resize(255),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.485, 0.456, 0.406],
+                                                           [0.229, 0.224, 0.225])])
+        
+    train_dataset = datasets.ImageFolder(train_dir, transform = train_transform)
+    test_dataset = datasets.ImageFolder(test_dir, transform = test_transform)
+        
+    trainloader = DataLoader(train_dataset, batch_size = 32, shuffle = True)
+    testloader = DataLoader(test_dataset, batch_size = 32)
+        
+    return trainloader, testloder
+    
+#My training loop function and print the result
+
+def train_funct(epochs=30, hidden_units=512, lr_rate=0.001,  model='densenet121', gpu='cpu', data_dir, save_dir=False):
+    #Defining and transfering the model into the gpu or cpu
+    
+    device = gpu
+    model = densenet121
+    model.to(device)
+    
+    #Freeze the parameters of the model and set new parameters
+    for param in model.parameters():
+        param.requires_grad = False
+    classifer = Classifier(hidden_units=hidden_units)
+    model.classifier = my_classifier()
+    
+    #Defining the  Criterion and the optimizer
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=lr_rate)
+    
+    #Loading the datasets and defining loaders using dataloader function
+    trainloader, validloader = data_loader(image_dir)
+    
+    epochs = epochs
+
+    train_losses, test_losses = [], []
+    for e in range(epochs):
+        running_loss = 0
+        for images, labels in trainloader:
+            images, labels = images.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            output = model(images)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        else:
+            valid_loss = 0
+            accuracy = 0 
+
+            model.eval()
+            with torch.no_grad():
+                for images, labels in validloader:
+                    images, labels = images.to(device), labels.to(device)
+
+                    output = model(images)
+                    test_loss += criterion(output, labels)
+
+                    ps = torch.exp(output)
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equality = top_class == labels.view(*top_class.shape)
+                    accuracy += torch.mean(equality.type(torch.FloatTensor))
+
+            model.train()
+
+            train_losses.append(running_loss/len(trainloader))
+            test_losses.append(test_loss/len(testloader))
+
+            print("Epoch: {}/{}.. ".format(e+1, epochs),
+                  "Trainning Loss: {:.3f}.. ".format(running_loss/len(trainloader)),
+                  "Valid Loss: {:.3f}.. ".format(valid_loss/len(validloader)),
+                  "Test Accuracy: {:.3f}.. ".format(accuracy/len(testloader)))
+
+
+#To save directory
+def saved_dir(saved_dir, model):
+    
+    checkpoint = {'state_dict': model.state_dict(),
+                 'opitmizer': optimizer.state_dict()}
+    torch.save(chekpoint, saved_dir)
+    
+    return saved_dir
+
+
+#Main function
+def main():
+    in_arg = get_input_arg()
+    epochs = in_arg.epochs
+    lr_rate = in_arg.learning_rate
+    data_dir = in_arg.image_dir
+    model_arch = in_arg.arch
+    save_dir  = in_arg.save_dir
+    hidden_units = in_arg.hidden_units
+    gpu = in_arg.gpu
+    
+    
+    if (epochs and lr_rate and hidden_units):
+        train_func(epochs, hidden_units, lr_rate, data_dir)
+    if gpu:
+        train_func(data_dir, gpu)
+    if save_dir:
+        saved_dir(save_dir, model)
+    train_func(data_dir)
+    
+if __name__ == "__main__":
+    main()
